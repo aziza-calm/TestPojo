c270cf2787eb1d05b5c7c62ddb850647d356669e
aziza-calm <aziza.kalmasova@phystech.edu>
2020-12-01 11:45:09 +0300

    streaming started: test touppercase

---
+++ src/main/java/Streaming.java
@@ -0,0 +1,35 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueMapper;
+
+import java.util.Properties;
+
+public class Streaming {
+    public static void main(String[] args) {
+        Properties config = new Properties();
+
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "hello-world");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("source-topic");
+        KStream<String, String> upperCasedStream = source.mapValues((ValueMapper<String, String>) String::toUpperCase);
+        upperCasedStream.to("sink-topic");
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+        //printed the topology
+        System.out.println(streams.toString());
+
+        // shutdown hook to correctly close the streams application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+}
